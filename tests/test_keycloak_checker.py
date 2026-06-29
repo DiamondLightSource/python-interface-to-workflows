b7f72bf6452c5cbafa638b78cb6f493ffefb236b
Matthew Carre <matthew.carre@diamond.ac.uk>
2026-06-29 12:00:13 +0000

    feat(api): adds a test for authentication

---
+++ tests/test_keycloak_checker.py
@@ -0,0 +1,37 @@
+from unittest.mock import MagicMock, patch
+
+from pytest import mark
+
+from python_interface_to_workflows.keycloak_checker import return_key
+
+
+@mark.parametrize("dev", [True, False])
+@patch("python_interface_to_workflows.keycloak_checker.KeycloakOpenID")
+@patch("python_interface_to_workflows.keycloak_checker.generate_code_verifier")
+@patch("python_interface_to_workflows.keycloak_checker.generate_code_challenge")
+@patch("python_interface_to_workflows.keycloak_checker._open_auth_url")
+def test_return_key(
+    mock_open_auth_url: MagicMock,
+    mock_gen_code_challenge: MagicMock,
+    mock_gen_code_verifier: MagicMock,
+    mock_gen_keycloak_id: MagicMock,
+    dev: bool,
+):
+    mock_gen_code_verifier.return_value = "verifier"
+    mock_gen_code_challenge.return_value = ("challenge", "S256")
+    mock_open_auth_url.return_value = "auth_url_code"
+
+    keycloak = MagicMock()
+    mock_gen_keycloak_id.return_value = keycloak
+
+    keycloak.auth_url.return_value = "https://mock.site"
+    keycloak.token.return_value = {"access_token": "fake_token"}
+    assert return_key(dev) == "fake_token"
+    mock_open_auth_url.assert_called_once_with("https://mock.site")
+    keycloak.token.assert_called_once_with(
+        grant_type="authorization_code",
+        code="auth_url_code",
+        redirect_uri="http://localhost:5173/",
+        code_verifier="verifier",
+        code_challenge="challenge",
+    )
--- tests/test_submit_to_graphql.py
+++ tests/test_submit_to_graphql.py
@@ -3,8 +3,10 @@ from unittest.mock import MagicMock, patch
 from python_interface_to_workflows.submit_to_graphql import submit_to_graphql
 
 
+@patch("python_interface_to_workflows.submit_to_graphql.return_key")
 @patch("python_interface_to_workflows.submit_to_graphql.Client")
-def test_submit_to_graphql(mock_client: MagicMock):
+def test_submit_to_graphql(mock_client: MagicMock, mock_key: MagicMock):
+    mock_key.return_value = "fake_token"
     mock_instance = MagicMock()
     mock_client.return_value = mock_instance
     mock_instance.execute.return_value = {
