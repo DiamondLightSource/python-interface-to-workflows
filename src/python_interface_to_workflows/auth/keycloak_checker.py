38514a88ac25dbb9c923e9beb175a9389bc48ff0
Matthew Carre <matthew.carre@diamond.ac.uk>
2026-06-30 15:18:42 +0000

    feat(api): adds environmental variables to ensure all code is ran in open_auth_url

--- src/python_interface_to_workflows/auth/keycloak_checker.py
+++ src/python_interface_to_workflows/auth/keycloak_checker.py
@@ -1,4 +1,8 @@
+import os
+
 from keycloak import KeycloakOpenID
+
+# from keycloak.exceptions import KeycloakPostError
 from keycloak.pkce_utils import generate_code_challenge, generate_code_verifier
 
 from python_interface_to_workflows.auth.open_auth_url import open_auth_url
@@ -8,7 +12,7 @@ def return_key(dev: bool) -> str:
     match dev:
         case True:
             keycloak_openid = KeycloakOpenID(
-                server_url="https://identity-test.diamond.ac.uk/",
+                server_url="https://identity.diamond.ac.uk/",
                 client_id="workflows-ui-dev",
                 realm_name="dls",
                 client_secret_key="",
@@ -22,7 +26,19 @@ def return_key(dev: bool) -> str:
                 client_secret_key="",
                 pool_maxsize=1,
             )
-
+    # btw, as you review this, I don't think this try-except is
+    # necessary - pretty certain this functionality is already in keycloak
+    # as the tokens i've seen are suspiciously similar and also don't require a login
+    # which i believe is due to it figuring out we already have a refresh token
+    # ergo, not confident that this hashed out stuff is needed.
+    # try:
+    #     newtoken: dict[str, str] = (  # pyright: ignore[reportUnknownVariableType]
+    #         keycloak_openid.refresh_token(  # pyright: ignore[reportUnknownMemberType]
+    #             os.environ.get("REFRESH")  # pyright: ignore[reportArgumentType]
+    #         )
+    #     )
+    #     return newtoken["access_token"]
+    # except KeycloakPostError:
     code_verifier = generate_code_verifier()
     code_challenge, code_challenge_method = generate_code_challenge(code_verifier)
     auth_url = keycloak_openid.auth_url(
@@ -32,20 +48,15 @@ def return_key(dev: bool) -> str:
         code_challenge=code_challenge,
         code_challenge_method=code_challenge_method,
     )
-    auth_code = open_auth_url(auth_url)
-    # save token and refresh token to file
-    # if they're present, check if access token is valid, if no but refresh token is
-    # , just refresh token
-    # if invalid, run whole thing
+    open_auth_url(auth_url)
     token: dict[str, str] = (  # pyright: ignore[reportUnknownVariableType]
         keycloak_openid.token(  # pyright: ignore[reportUnknownMemberType]
             grant_type="authorization_code",
-            code=auth_code,
+            code=os.environ["AUTH"],
             redirect_uri="http://localhost:5173/",
             code_verifier=code_verifier,
         )
     )
+    os.environ["REFRESH"] = token["refresh_token"]
+    os.environ["TOKEN"] = token["access_token"]
     return token["access_token"]  # pyright: ignore[reportUnknownArgumentType]
-
-
-print(return_key(dev=True))
