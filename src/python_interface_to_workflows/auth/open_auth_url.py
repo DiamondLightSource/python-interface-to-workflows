39d339c492dfa07f8ca090d2b2b7438d0cdbc35d
Matthew Carre <matthew.carre@diamond.ac.uk>
2026-06-30 08:29:27 +0000

    feat(api): adds error-handling to keycloak_checker

---
+++ src/python_interface_to_workflows/auth/open_auth_url.py
@@ -0,0 +1,41 @@
+import os
+import urllib.parse
+import webbrowser
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from typing import cast
+
+
+def open_auth_url(auth_url: str):
+
+    class _ReusingHTTPServer(HTTPServer):
+        allow_reuse_address = True
+        auth_code: str
+
+    class CallbackHandler(BaseHTTPRequestHandler):
+        def do_GET(self):
+            query = urllib.parse.urlparse(self.path).query
+            params = urllib.parse.parse_qs(query)
+            if "code" in params:
+                cast(_ReusingHTTPServer, self.server).auth_code = params["code"][0]
+                self.send_response(200)
+                self.end_headers()
+                self.wfile.write(
+                    b"Authorization successful. You can close this window."
+                )
+            else:
+                self.send_response(400)
+                self.end_headers()
+                self.wfile.write(b"Missing authorization code.")
+
+    # try:
+    httpd = _ReusingHTTPServer(("localhost", 5173), CallbackHandler)
+    webbrowser.open(auth_url)
+    httpd.handle_request()  # Wait for one request
+    os.environ["AUTH"] = httpd.auth_code
+    httpd.server_close()
+    exit()
+    # except OSError:
+    # os.environ["AUTH"] = ""
+    # print("OSERROR")
+    # time.sleep(2)
+    # exit()
