251f9d494a27baa209d2b3e167b6738e3e3eab0d
Matthew Carre <matthew.carre@diamond.ac.uk>
2026-07-15 09:49:44 +0000

    feat(copy): adds makecopiercorrect.sh, making sure the copy template is up to date

---
+++ scripts/makecopiercorrect.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cd "src/python_interface_to_workflows/workflow_definitions"
+
+cp *.py "../../copier_template/src/{{ project_name }}/workflow_definitions"
+cp ../templates/*.yaml "../../copier_template/src/{{ project_name }}/templates"
+
+for file in "../../copier_template/src/{{ project_name }}/workflow_definitions"/*
+do
+    [[ $file == *.jinja ]] && continue
+    sed -i 's/python-interface-to-workflows/{{repo_name}}/g' "$file"
+    sed -i 's/DiamondLightSource/{{github_org}}/g' "$file"
+    mv "$file" "$file.jinja"
+done
+
+for file in "../../copier_template/src/{{ project_name }}/templates"/*
+do
+    [[ $file == *.jinja ]] && continue
+    sed -i 's/python-interface-to-workflows/{{repo_name}}/g' "$file"
+    sed -i 's/DiamondLightSource/{{github_org}}/g' "$file"
+    mv "$file" "$file.jinja"
+done
+git add "../../copier_template/src/{{ project_name }}/workflow_definitions"/*
+git add "../../copier_template/src/{{ project_name }}/templates"/*
