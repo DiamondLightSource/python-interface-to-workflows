f0ac45ec8a4d7442d74fec16ce056c47ee053775
Matthew Carre <matthew.carre@diamond.ac.uk>
2026-07-21 09:52:28 +0000

    feat(copier): updates copier template to new standards

--- scripts/makecopiercorrect.sh
+++ scripts/makecopiercorrect.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "src/python_interface_to_workflows/workflow_definitions"
 
-cp *.py "../../copier_template/src/{{ project_name }}/workflow_definitions"
+cp *example*.py "../../copier_template/src/{{ project_name }}/workflow_definitions"
 cp ../templates/*.yaml "../../copier_template/src/{{ project_name }}/templates"
 
 for file in "../../copier_template/src/{{ project_name }}/workflow_definitions"/*
