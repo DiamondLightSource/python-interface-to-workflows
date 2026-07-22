edbdef01046bc51071ad57838be787bcbe3f5a2d
Matthew Carre <matthew.carre@diamond.ac.uk>
2026-07-22 15:01:29 +0000

    test(auth): updates unit tests

--- src/copier_template/src/{{ project_name }}/auth/open_auth_url.py
+++ src/copier_template/src/{{ project_name }}/auth/open_auth_url.py
@@ -1,9 +1,12 @@
 import os
 import socket
+import time
 import urllib.parse
 import webbrowser
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from typing import cast
+
+import dotenv
 
 
 class _ReusingHTTPServer(HTTPServer):
@@ -26,16 +29,25 @@ class CallbackHandler(BaseHTTPRequestHandler):
             self.wfile.write(b"Missing authorization code.")
 
 
-def open_auth_url(auth_url: str) -> None:
-    httpd = _ReusingHTTPServer(("localhost", 8000), CallbackHandler)
-    webbrowser.open(auth_url)
-    try:
-        httpd.handle_request()
-        os.environ["AUTH"] = httpd.auth_code
-    except OSError:
-        os.environ["AUTH"] = ""
-        print("ERROR: Port in use. Please restart your terminal.")
-        exit(1)
-    finally:
-        httpd.socket.shutdown(socket.SHUT_RDWR)
-        httpd.server_close()
+def open_auth_url(auth_url: str, port: int) -> bool:
+    dotenv.load_dotenv(dotenv_path="src/.env", override=True)
+    expiry_str: str = os.environ.get("EXPIRY")  # pyright: ignore
+    print(f"expiry_str: {expiry_str}", str(time.time()))
+    if (expiry_str == "" or int(expiry_str)) <= float(time.time()):
+        httpd = _ReusingHTTPServer(("localhost", port), CallbackHandler)
+        webbrowser.open(auth_url)
+        try:
+            httpd.handle_request()
+            os.environ["AUTH"] = httpd.auth_code
+            dotenv.set_key("src/.env", "AUTH", httpd.auth_code)
+        except OSError:
+            os.environ["AUTH"] = ""
+            print("ERROR: Port in use. Please restart your terminal.")
+            exit(1)
+        finally:
+            httpd.socket.shutdown(socket.SHUT_RDWR)
+            httpd.server_close()
+        return True
+    else:
+        print("using refresh token")
+        return False
