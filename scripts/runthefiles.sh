0c64336edcaeb31cd28a07ffdd3d8cc2355f254e
Matthew Carre <matthew.carre@diamond.ac.uk>
2026-08-06 13:13:24 +0000

    docs(copier): updates copier and runthefiles.sh

--- scripts/runthefiles.sh
+++ scripts/runthefiles.sh
@@ -3,6 +3,10 @@ cd src/python_interface_to_workflows/workflow_definitions
 for file in *
 do
 [[ -d "$file" ]] && continue
+    if grep -Eq '^\s*(from|import)\s+python_workflow_submitter\b' "$file"; then
+        echo "ERROR: $file imports submitter"
+        exit 1
+    fi
 uv run "$file"
 done
 mv *.txt ../templates/
